16ccda738002dcfb9995110d62ac94d95f18f060
janenotjung-hue <107402425+janenotjung-hue@users.noreply.github.com>
2024-09-24 18:39:23 +1200

    ai speech app

--- run.sh
+++ run.sh
@@ -14,6 +14,6 @@
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
 ## Examples of other valid options; For more, please view the user guide
-### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
+java -jar RepoSense.jar --repos https://github.com/flynncupitt/AI-Speech-App
 
-java -jar RepoSense.jar --config ./configs
+#java -jar RepoSense.jar --config ./configs
